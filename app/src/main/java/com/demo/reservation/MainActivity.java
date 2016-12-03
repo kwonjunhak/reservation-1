2d9bc8789a82492a6e41aeb581f4b68c36f064f6
pakjkwan2 <pakjkwan2@gmail.com>
2016-12-03 11:58:37 +0900

    final commit

--- app/src/main/java/com/demo/reservation/MainActivity.java
+++ app/src/main/java/com/demo/reservation/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView tv_today;
     RadioButton rd_date,rd_time;
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TimePicker time;
     Button bt_reserve;
     TextView tv_year,tv_month,tv_day,tv_hour,tv_min;
+    String date="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +38,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rd_date.setOnClickListener(this);
         rd_time.setOnClickListener(this);
         bt_reserve.setOnClickListener(this);
+        /*
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date d = new Date();
+        String today = sdf.format(d);
+        tv_today.setText("TODAY : "+today);
+        */
+        tv_today.setText("TODAY : "+new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+        cal.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                date = year +"-"+(month+1)+"-"+day;
+            }
+        });
         time.setVisibility(View.INVISIBLE);
         cal.setVisibility(View.INVISIBLE);
     }
@@ -49,7 +66,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 time.setVisibility(View.VISIBLE);
                 cal.setVisibility(View.INVISIBLE);
                 break;
-            case R.id.bt_reserve:break;
+            case R.id.bt_reserve:
+                String arr[]= date.split("-");
+                tv_year.setText(arr[0]);
+                tv_month.setText(arr[1]);
+                tv_day.setText(arr[2]);
+                tv_hour.setText(Integer.toString(time.getHour()));
+                tv_min.setText(Integer.toString(time.getMinute()));
+                break;
         }
     }
 }
